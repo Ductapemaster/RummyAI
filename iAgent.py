1edb3532b1a34a917a2528ce6ab367072eb76acd
Ryan Bahneman <RyanBahneman@gmail.com>
2013-09-07 13:39:58 -0700

    adding definitin of drawing

--- iAgent.py
+++ iAgent.py
@@ -8,6 +8,9 @@ class IAgent():
 		pass
 
 	def getDrawAction(self):
+		#return an integer
+		# 0 to draw off of the draw pile
+		# N to pickup N cards from the discard pile
 		pass
 
 	def getMeldActions(self):
