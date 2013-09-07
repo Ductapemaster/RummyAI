44b3d45ac5b6275f39732c96cf5edc751665193f
Dan Kouba <Ductapemaster@gmail.com>
2013-09-07 12:40:02 -0700

    Meld now checks if it is a valid independent meld

--- Meld_tests.py
+++ Meld_tests.py
@@ -40,7 +40,46 @@ class MeldTests(unittest.TestCase):
 		self.assertEqual(m.meld_type, 0)
 		self.assertEqual(m.isIndependentMeld(), False)
 		
+	def test_sorting_with_off_suit_straight(self):
+		test_cards = [Card(9,1), Card(7,4), Card(8,3)]
+		test_cards_sorted = [Card(9,1), Card(8,3), Card(7,4)]
+		test_value = 15
+		m = Meld(test_cards)
+		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(m.isIndependentMeld(), False)
 		
+	def test_sorting_with_suited_straight(self):
+		test_cards = [Card(9,1), Card(7,1), Card(8,1)]
+		test_cards_sorted = [Card(7,1), Card(8,1), Card(9,1)]
+		test_value = 15
+		m = Meld(test_cards)
+		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(m.isIndependentMeld(), True)
+		
+	def test_sorting_with_three_of_a_kind(self):
+		test_cards = [Card(13,4), Card(13,1), Card(13,2)]
+		test_cards_sorted = [Card(13,1), Card(13,2), Card(13,4)]
+		test_value = 30
+		m = Meld(test_cards)
+		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(m.isIndependentMeld(), True)
+		
+	def test_independent_meld_method_with_four_of_a_kind(self):
+		test_cards = [Card(1,1), Card(1,2), Card(1,3), Card(1,4)]
+		test_value = 60
+		m = Meld(test_cards)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(m.isIndependentMeld(), True)
+		
+	def test_independent_meld_with_five_card_straight(self):
+		test_cards = [Card(1,1), Card(2,1), Card(3,1), Card(4,1), Card(5,1)]
+		test_value = 35
+		m = Meld(test_cards)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(m.isIndependentMeld(), True)	
 
 if __name__ == '__main__':
 	unittest.main()
--- meld.py
+++ meld.py
@@ -1,5 +1,5 @@
 from card import *
-from operator import *
+from operator import attrgetter
 
 class Meld:
 	def __init__(self, card_list=[]):
@@ -13,8 +13,35 @@ class Meld:
 		self.meld_type = 0
 		
 	def isIndependentMeld(self):
-		if len(card_list) < 3: return False
-		else: return True
+		if len(self.card_list) < 3: 
+			return False
+		else:
+			
+			same_rank = True
+			for c in self.card_list:
+				if c.rank != self.card_list[0].rank: 
+					same_rank = False
+					break
+			
+			same_suit = True
+			for c in self.card_list:
+				if c.suit != self.card_list[0].suit: 
+					same_suit = False
+					break
+					
+			is_straight = True
+			for idx in range(len(self.card_list)):
+				if idx != 0:
+					if self.card_list[idx].rank != self.card_list[idx-1].rank + 1:
+						is_straight = False
+						break
+			
+			if same_rank:
+				return True
+			elif (same_suit and is_straight):
+				return True
+			else: 
+				return False
 		
 	def getMeldValue(self):
 		points = 0
