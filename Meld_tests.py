7efd7a8118474dc7acb13c834595feccdd998888
Dan Kouba <Ductapemaster@gmail.com>
2013-09-07 11:50:37 -0700

    Initial tests working, need new Card class to continue testing

--- Meld_tests.py
+++ Meld_tests.py
@@ -1,12 +1,46 @@
 import unittest
+	# properties
+		# list of cards
+		# number of points
+		# flag to specify meld type
+		# isIndependentMeld flag
+	
+	# methods
+		# add card
 
-class CardSetTests(unittest.TestCase):
+from card import *	
+from meld import *
+
+class MeldTests(unittest.TestCase):
 
 	def setUp(self):
 		pass
 
-	def test_constructor(self):
-		pass
+	def test_constructor_with_empty_meld(self):
+		m = Meld()
+		self.assertEqual(m.getMeldValue(), 0)
+		self.assertEqual(len(m.card_list), 0)
+		self.assertEqual(m.meld_type, 0)
+		self.assertEqual(m.isIndependentMeld(), False)
+		
+	def test_contructor_with_one_card_meld(self):
+		test_cards = [Card(4, 2)]
+		m = Meld(test_cards)
+		self.assertEqual(m.getMeldValue(), test_cards[0].numPoints())
+		self.assertEqual(len(m.card_list), 1)
+		self.assertEqual(m.meld_type, 0)
+		self.assertEqual(m.isIndependentMeld(), False)
+		
+	def test_constructor_with_three_card_meld(self):
+		test_cards = [Card(7,1), Card(4,3), Card(12,4)]
+		test_value = 20
+		m = Meld(test_cards)
+		self.assertEqual(m.getMeldValue(), test_value)
+		self.assertEqual(len(m.card_list), 3)
+		self.assertEqual(m.meld_type, 0)
+		self.assertEqual(m.isIndependentMeld(), False)
+		
+		
 
 if __name__ == '__main__':
 	unittest.main()
--- meld.py
+++ meld.py
@@ -1,5 +1,23 @@
-from cardCollection import *
+from card import *
+from operator import *
 
-class CardSet(self):
-	def __init__(self):
-		pass
+class Meld:
+	def __init__(self, card_list=[]):
+		
+		self.card_list = sorted(card_list, key=attrgetter('suit', 'rank'))
+		
+		# Meld Type:
+		# 0: don't care (play anywhere valid)
+		# 1: set
+		# 2: run
+		self.meld_type = 0
+		
+	def isIndependentMeld(self):
+		if len(card_list) < 3: return False
+		else: return True
+		
+	def getMeldValue(self):
+		points = 0
+		for c in self.card_list:
+			points += c.numPoints()
+		return points
