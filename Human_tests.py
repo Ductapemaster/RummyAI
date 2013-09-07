668a5229905c8393248d6fdf4000a54f3c5d2d48
Ryan Bahneman <RyanBahneman@gmail.com>
2013-09-06 17:18:12 -0700

    adding tests and gitignore file

---
+++ Human_tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from Card import *
+
+class CardTests(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_constructor_works_as_expected(self):
+		rank = 1
+		suit = 2
+		c = Card(rank, suit)
+		self.assertEqual(c.rank, rank)
+		self.assertEqual(c.suit, suit)
+
+if __name__ == '__main__':
+	unittest.main()
+
