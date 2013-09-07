110b30e7a4482a2f8a38565ab2fd125a2b93d9bb
Dan Kouba <Ductapemaster@gmail.com>
2013-09-06 21:39:31 -0700

    - Made Deck class a subclass of the CardCollection class - Rewrote Deck class tests to support change

--- Deck_tests.py
+++ Deck_tests.py
@@ -14,20 +14,13 @@ class CardTests(unittest.TestCase):
 			for rank in range (1, 14):
 				test_deck.append(Card(rank, suit))
 		d = Deck()
-		self.assertEqual(d.cardsLeft(), 52)
-		self.assertEqual(d.deck.cards, test_deck)
+		self.assertEqual(d.numCards(), 52)
+		self.assertEqual(d.cards, test_deck)
 		
 	def test_shuffle(self):
 		d = Deck()
 		d.shuffle()
-		self.assertEqual(d.cardsLeft(), 52)
-		
-	def test_get_top_card(self):
-		d = Deck()
-		d.shuffle()
-		top_card_test = d.deck.cards[0]
-		self.assertEqual(d.getTopCard(), top_card_test)
-		self.assertEqual(d.cardsLeft(), 51)
+		self.assertEqual(d.numCards(), 52)
 
 if __name__ == '__main__':
 	unittest.main()
--- deck.py
+++ deck.py
@@ -1,26 +1,14 @@
 import random
 from cardCollection import *
 
-class Deck:
+# Inherits from CardCollection class, adds shuffle capability
+class Deck(CardCollection):
 	def __init__(self):
-		self.deck = CardCollection()
+		super().__init__()
 		for suit in range(1, 5):
 			for rank in range(1,14):
-				self.deck.addCard(Card(rank, suit))
+				self.addCard(Card(rank, suit))
 				
-	def __repr__(self):
-		outputStr = ""
-		for card in self.deck.cards:
-			outputStr += card.__repr__()
-			outputStr += "\n"
-		return outputStr
-	
 	def shuffle(self):
-		random.shuffle(self.deck.cards)
-	
-	# Pop off the top card of the deck and return it
-	def getTopCard(self):
-		return self.deck.popCard()
-		
-	def cardsLeft(self):
-		return self.deck.numCards()
+		random.shuffle(self.cards)
+
