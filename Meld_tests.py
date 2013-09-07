98a93a3d051b38270fa43f3f4a60a59fd39726a4
Dan Kouba <Ductapemaster@gmail.com>
2013-09-07 13:33:06 -0700

    Meld Class Modifications: - Split up same suit/rank and straight checking into methods - Added isValidMeld() method to check if melds smaller than 3 cards follow the rules - Tests to support added functions

--- Meld_tests.py
+++ Meld_tests.py
@@ -1,12 +1,4 @@
 import unittest
-	# properties
-		# list of cards
-		# number of points
-		# flag to specify meld type
-		# isIndependentMeld flag
-	
-	# methods
-		# add card
 
 from card import *	
 from meld import *
@@ -19,7 +11,7 @@ class MeldTests(unittest.TestCase):
 	def test_constructor_with_empty_meld(self):
 		m = Meld()
 		self.assertEqual(m.getMeldValue(), 0)
-		self.assertEqual(len(m.card_list), 0)
+		self.assertEqual(len(m.cards), 0)
 		self.assertEqual(m.meld_type, 0)
 		self.assertEqual(m.isIndependentMeld(), False)
 		
@@ -27,7 +19,7 @@ class MeldTests(unittest.TestCase):
 		test_cards = [Card(4, 2)]
 		m = Meld(test_cards)
 		self.assertEqual(m.getMeldValue(), test_cards[0].numPoints())
-		self.assertEqual(len(m.card_list), 1)
+		self.assertEqual(len(m.cards), 1)
 		self.assertEqual(m.meld_type, 0)
 		self.assertEqual(m.isIndependentMeld(), False)
 		
@@ -36,7 +28,7 @@ class MeldTests(unittest.TestCase):
 		test_value = 20
 		m = Meld(test_cards)
 		self.assertEqual(m.getMeldValue(), test_value)
-		self.assertEqual(len(m.card_list), 3)
+		self.assertEqual(len(m.cards), 3)
 		self.assertEqual(m.meld_type, 0)
 		self.assertEqual(m.isIndependentMeld(), False)
 		
@@ -45,7 +37,7 @@ class MeldTests(unittest.TestCase):
 		test_cards_sorted = [Card(9,1), Card(8,3), Card(7,4)]
 		test_value = 15
 		m = Meld(test_cards)
-		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.cards, test_cards_sorted)
 		self.assertEqual(m.getMeldValue(), test_value)
 		self.assertEqual(m.isIndependentMeld(), False)
 		
@@ -54,7 +46,7 @@ class MeldTests(unittest.TestCase):
 		test_cards_sorted = [Card(7,1), Card(8,1), Card(9,1)]
 		test_value = 15
 		m = Meld(test_cards)
-		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.cards, test_cards_sorted)
 		self.assertEqual(m.getMeldValue(), test_value)
 		self.assertEqual(m.isIndependentMeld(), True)
 		
@@ -63,7 +55,7 @@ class MeldTests(unittest.TestCase):
 		test_cards_sorted = [Card(13,1), Card(13,2), Card(13,4)]
 		test_value = 30
 		m = Meld(test_cards)
-		self.assertEqual(m.card_list, test_cards_sorted)
+		self.assertEqual(m.cards, test_cards_sorted)
 		self.assertEqual(m.getMeldValue(), test_value)
 		self.assertEqual(m.isIndependentMeld(), True)
 		
@@ -79,7 +71,44 @@ class MeldTests(unittest.TestCase):
 		test_value = 35
 		m = Meld(test_cards)
 		self.assertEqual(m.getMeldValue(), test_value)
-		self.assertEqual(m.isIndependentMeld(), True)	
+		self.assertEqual(m.isIndependentMeld(), True)
+
+	def test_can_combine_with_method_with_valid_three_plus_one_of_a_kind(self):
+		test_cards_1 = [Card(5,1), Card(5,2), Card(5,4)]
+		test_cards_2 = [Card(5,3)]
+		m = Meld(test_cards_1)
+		self.assertEqual(m.canCombineWith(test_cards_2), True)
+		
+	def test_can_combine_with_method_with_valid_three_plus_two_straight(self):
+		test_cards_1 = [Card(2,1), Card(1,1), Card(3,1)]
+		test_cards_2 = [Card(5,1), Card(4,1)]
+		m = Meld(test_cards_1)
+		self.assertEqual(m.canCombineWith(test_cards_2), True)
+		
+	def test_can_combine_with_method_with_invalid_three_plus_one_of_a_kind(self):
+		test_cards_1 = [Card(5,1), Card(5,2), Card(5,4)]
+		test_cards_2 = [Card(11,3)]
+		m = Meld(test_cards_1)
+		self.assertEqual(m.canCombineWith(test_cards_2), False)
+		
+	def test_can_combine_with_method_with_invalid_three_plus_two_straight(self):
+		test_cards_1 = [Card(1,1), Card(2,1), Card(3,1)]
+		test_cards_2 = [Card(4,1), Card(6,1)]
+		m = Meld(test_cards_1)
+		self.assertEqual(m.canCombineWith(test_cards_2), False)
+	
+	def test_can_combine_with_method_with_invalid_run_of_two(self):
+		test_card_1 = [Card(1,2)]
+		test_card_2 = [Card(2,1)]
+		m = Meld(test_card_1)
+		self.assertEqual(m.canCombineWith(test_card_2), False)
+		
+	def test_can_combine_with_method_with_valid_run_of_two(self):
+		test_card_1 = [Card(12,2)]
+		test_card_2 = [Card(13,2)]
+		m = Meld(test_card_1)
+		self.assertEqual(m.canCombineWith(test_card_2), True)
+		
 
 if __name__ == '__main__':
 	unittest.main()
