fd6211057e9d1f999573bc1b4b9d72d9a9556a9a
Dan Kouba <Ductapemaster@gmail.com>
2013-09-07 14:19:31 -0700

    Basic functions of Human laid out

--- Human_tests.py
+++ Human_tests.py
@@ -8,11 +8,6 @@ class HumanTests(unittest.TestCase):
 		pass
 
 	def test_constructor_works_as_expected(self):
-		rank = 1
-		suit = 2
-		c = Card(rank, suit)
-		self.assertEqual(c.rank, rank)
-		self.assertEqual(c.suit, suit)
 
 if __name__ == '__main__':
 	unittest.main()
