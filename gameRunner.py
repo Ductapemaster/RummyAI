5bb83fbc09035f8af5fe9cf63aa544452a571b07
Ryan Bahneman <RyanBahneman@gmail.com>
2013-09-06 21:30:39 -0700

    fleshing out more of the game runner. specifically the playGame() method

--- gameRunner.py
+++ gameRunner.py
@@ -1,12 +1,15 @@
+import random
+
 from iAgent import *
 from game import *
+
 
 class GameRunner():
 
 	def __init__(self):
 		pass
 
-	def createNewGame(self, agents):
+	def createNewGame(self, agents, starting_player=1):
 		self.num_players = len(agents)
 
 		if self.num_players < 2:
@@ -15,3 +18,49 @@ class GameRunner():
 		self.agents = agents
 
 		self.game = Game(self.num_players)
+
+		self.starting_player = starting_player
+		self.winner = -1
+		
+		
+
+
+	def playGame(self):
+
+		cur_player = self.starting_player
+		
+		while True:
+
+			draw = self.agents[cur_player].getDrawAction()
+
+			# verify draw is vaild
+
+			# make the draw action happen
+
+			meld = self.agents[cur_player].getMeldAction()
+
+			# verify meld is vaild
+
+			# make the meld action happen
+
+			self.agents[cur_player].getDiscardAction() 
+
+			# verify discard is vaild
+
+			# make the discard action happen
+
+
+			# if self.game.player[cur_player].hand.size() == 0:
+				#self.winner = cur_player
+				#return self.winner
+
+
+			# let the next player go
+			cur_player = (cur_player + 1) % self.num_players
+
+			# temporary, to prevent infinite loop
+			break
+
+
+			
+
