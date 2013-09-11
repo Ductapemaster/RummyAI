c1f9ee09a0db235affa085ced8e0f5d127741a8f
Ryan Bahneman <RyanBahneman@gmail.com>
2013-09-11 07:38:58 -0700

    fixing issue

--- card.py
+++ card.py
@@ -3,7 +3,7 @@
 class Card:
 
 	# Define face cards
-	face_cards = {-1: 'NUL', 1:'AL ', 11:'J  ', 12:'Q  ', 13:'K  ', 14:'AH ', 15:'JKR'}
+	face_cards = {-1: 'NUL', 1:'AL ', 11:'J  ', 12:'Q  ', 13:'K  ', 14:'AH ', 20:'JKR'}
 	# Define suits, 0 is a null suit
 	suits = {-1: 'N', 1: 'S', 2: 'H', 3: 'C', 4: 'D'}
 
@@ -18,7 +18,7 @@ class Card:
 		# Return face card rank if in dict, otherwise return string of the rank (2-9)
 		readable_rank = self.face_cards.get(self.rank, str(self.rank))
 		readable_alt_rank = self.face_cards.get(self.alt_rank, str(self.alt_rank))
-		readable_suit = suits.get(self.suit)
+		readable_suit = self.suits.get(self.suit)
 		return "<%s %s %s>" % (readable_rank, readable_alt_rank, readable_suit)
 		
 	def __eq__(self, other):
