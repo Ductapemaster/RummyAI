96fe0c7fd0a0c0a7d64bf827204a3ee5b82f9128
Ryan Bahneman <RyanBahneman@gmail.com>
2013-09-11 06:57:27 -0700

    updating descriptions

--- iAgent.py
+++ iAgent.py
@@ -8,13 +8,21 @@ class IAgent():
 		pass
 
 	def getDrawAction(self):
-		#return an integer
-		# 0 to draw off of the draw pile
-		# N to pickup N cards from the discard pile
+		# Return an integer
+		#	0 to draw off of the draw pile
+		#	N to pickup N cards from the discard pile
+		#		You must meld the last card you pick up
+		#		If you do not, you lose immediately, cheater
+		
 		pass
 
 	def getMeldActions(self):
+		# Return a list of meld objects, or an empty list
+		#	the melds must consist of cards in your hand
 		pass
 	
 	def getDiscardAction(self):
+		# Return a card that exists in your hand
+		#	If the card does not exist in your hand
+		#	you lose immediately, cheater
 		pass
